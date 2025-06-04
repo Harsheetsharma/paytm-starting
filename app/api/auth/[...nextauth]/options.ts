e57307724faba4e2d19c862554afaed4867077a1
Harsheetsharma <harshitsharma8012@gmail.com>
2025-06-04 15:05:21 +0530

    user-app done

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,75 @@
+import { NextAuthOptions } from "next-auth";
+import GitHubProvider from 'next-auth/providers/github';
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import { prisma } from "../../../../db";
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: 'credentials',
+            credentials: {
+                email: { label: "Phone number", type: "text", placeholder: "1231231231", required: true },
+                password: { label: "Password", type: "password", required: true }
+            },
+            async authorize(credentials: any) {
+                const hashedPassword = await bcrypt.hash(credentials.password, 10);
+                const existingUser = await prisma.user.findFirst({
+                    where: {
+                        number: credentials.email
+                    }
+                });
+
+                if (existingUser) {
+                    const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password);
+                    if (passwordValidation) {
+                        return {
+                            id: existingUser.id.toString(),
+                            name: existingUser.name,
+                            email: existingUser.number
+                        }
+                    }
+                    return null;
+                }
+
+                try {
+                    const user = await prisma.user.create({
+                        data: {
+                            number: credentials.email,
+                            password: hashedPassword
+                        }
+                    });
+
+                    return {
+                        id: user.id.toString(),
+                        name: user.name,
+                        email: user.number
+                    }
+                } catch (e) {
+                    console.error(e);
+                }
+
+                return null
+            }
+        }),
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string
+        }),
+    ],
+    // callbacks: {
+    //     async jwt({ token, user }) {
+    //         if (user) {
+    //             token.username = (user as any).username
+    //         }
+    //         return token;
+    //     },
+    //     async session({ session, token }) {
+    //         if (token?.username) {
+    //             (session.user as any).username = token.username;
+
+    //         }
+    //         return session;
+    //     }
+    // }
+}
